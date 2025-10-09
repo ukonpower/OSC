e7aad8380ae398f6a53eacd6fe840c4de389af3f
ukonpower <ore.ukonpower@gmail.com>
2025-10-09 10:01:31 +0900

    refactor: Remove DemoMusic, OREngineCubeMaterial, OREngineLogoMaterial, SkyBox, and SPZModel components along with their associated shaders and references from the project

--- src/ts/Resources/_data/componentList.ts
+++ src/ts/Resources/_data/componentList.ts
@@ -8,15 +8,10 @@ import { OverlayMixer } from '../Components/Camera/MainCamera/PostProcess/Overla
 import { PixelSort } from '../Components/Camera/MainCamera/PostProcess/PixelSort/index.ts';
 import { MainCamera } from '../Components/Camera/MainCamera/index.ts';
 import { Music } from '../Components/Demo4/Music/index.ts';
-import { DemoMusic } from '../Components/DemoProject/DemoMusic/index.ts';
-import { OREngineCubeMaterial } from '../Components/DemoProject/OREngineCubeMaterial/index.ts';
-import { OREngineLogoMaterial } from '../Components/DemoProject/OREngineLogoMaterial/index.ts';
-import { SkyBox } from '../Components/DemoProject/SkyBox/index.ts';
 import { ShakeViewer } from '../Components/ObjectControls/CameraShake/index.ts';
 import { LookAt } from '../Components/ObjectControls/LookAt/index.ts';
 import { ObjectRotate } from '../Components/ObjectControls/ObjectRotate/index.ts';
 import { OrbitControls } from '../Components/ObjectControls/OrbitControls/index.ts';
-import { SPZModel } from '../Components/Samples/SPZModel/index.ts';
 import { TextureGenerator } from '../Components/Texture/TextureGenerator/index.ts';
 import { BLidgeClient } from '../Components/Utilities/BLidgeClient/index.ts';
 import { UniformControls } from '../Components/Utilities/UniformsControls/index.ts';
@@ -28,20 +23,11 @@ export const COMPONENTLIST: {[key: string]: any} = {
 	Demo4: {
 		Music,
 	},
-	DemoProject: {
-		DemoMusic,
-		OREngineCubeMaterial,
-		OREngineLogoMaterial,
-		SkyBox,
-	},
 	ObjectControls: {
 		ShakeViewer,
 		LookAt,
 		ObjectRotate,
 		OrbitControls,
-	},
-	Samples: {
-		SPZModel,
 	},
 	Texture: {
 		TextureGenerator,
