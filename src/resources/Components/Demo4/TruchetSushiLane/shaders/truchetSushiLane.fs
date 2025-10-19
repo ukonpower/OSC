9b2791728a1547035142f81903618ce387dfd18b
ukonpower <ore.ukonpower@gmail.com>
2025-10-19 16:47:04 +0900

    feat: Enhance TruchetSushiLane shader with oriented box and distance functions
    - Added `sdOrientedBox` function to calculate signed distance for oriented boxes.
    - Introduced new distance functions `p1`, `p2`, and `p3` for improved shape rendering.
    - Updated grid calculations to support variable grid sizes, enhancing procedural generation.
    - Refined distance calculations for better visual fidelity in the Truchet pattern rendering.

--- src/resources/Components/Demo4/TruchetSushiLane/shaders/truchetSushiLane.fs
+++ src/resources/Components/Demo4/TruchetSushiLane/shaders/truchetSushiLane.fs
@@ -5,29 +5,52 @@
 #include <hash>
 #include <rm_h>
 
-// float d1( vec3 d ) {
+float sdOrientedBox( in vec2 p, in vec2 a, in vec2 b, float th )
+{
+    float l = length(b-a);
+    vec2  d = (b-a)/l;
+    vec2  q = (p-(a+b)*0.5);
+          q = mat2(d.x,-d.y,d.y,d.x)*q;
+          q = abs(q)-vec2(l,th)*0.5;
+    return length(max(q,0.0)) + min(max(q.x,q.y),0.0);    
+}
 
-// 	return 0.0;
+float strokeWidth = 0.1;
 
-// }
+float p1( vec3 p, vec2 dir1 ) {
 
-// float d2( vec3 d ) {
+	float d = sdOrientedBox( p.xz, vec2( 0.0 ), dir1, strokeWidth );
+	return d;
 
-// 	return 0.0;
+}
+
+float p2( vec3 p, vec2 dir1, vec2 dir2 ) {
+
+	float d = sdOrientedBox( p.xz, vec2( 0.0 ), dir1, strokeWidth );
+	d = min( d, sdOrientedBox( p.xz, vec2( 0.0 ), dir2, strokeWidth ) );
+	return d;
 	
-// }
+}
 
-// float d3( vec3 d ) {
+float p3( vec3 p, vec2 dir1, vec2 dir2, vec2 dir3 ) {
 
-// 	return 0.0;
+	float d = sdOrientedBox( p.xz, vec2( 0.0 ), dir1, strokeWidth );
+	d = min( d, sdOrientedBox( p.xz, vec2( 0.0 ), dir2, strokeWidth ) );
+	d = min( d, sdOrientedBox( p.xz, vec2( 0.0 ), dir3, strokeWidth ) );
+
+	return d;
 	
-// }
+}
+
+float gridSize = 9.0;
 
 // SDF（Signed Distance Function）
 SDFResult D( vec3 p ) {
 
-	vec2 gridCenter = floor( p.xz ) + 0.5;
-    p.xz = mod( p.xz, 1.0 ) - 0.5;
+	vec3 op = p;
+
+	vec2 gridCenter = floor( p.xz / gridSize ) * gridSize + gridSize * 0.5;
+    p.xz = mod( p.xz, gridSize ) - gridSize * 0.5;
 
 	// TruchetTiling
 	// thanks to renard
@@ -57,38 +80,42 @@ SDFResult D( vec3 p ) {
 
 	float s = 0.0;
 
-	if( qCount == 0 ) {
+	float dist2D = 9999.0;
 
-		s = 0.1;
+	if( qCount == 0 ) {
 
 	}
 
 	if( qCount == 1 ) {
 
-		s = 0.2;
-		
+		dist2D = p1( p / gridSize, quv[0] );
+
 	}
 
 	if( qCount == 2 ) {
 
-		s = 0.3;
+		dist2D = p2( p / gridSize, quv[0], quv[1] );
 		
 	}
 
 	if( qCount == 3 ) {
 		
-		s = 0.4;
+		dist2D = p3( p / gridSize, quv[0], quv[1], quv[2] );
 
 	}
 
 	if( qCount == 4 ) {
 
-		s = 0.5;
+		dist2D = p3( p / gridSize, quv[1], quv[2], quv[3] );
 		
 	}
 
 	// とりあえずシンプルな球体
-	float d = length( p ) - s;
+	// float d = length( p ) - s;
+
+	float h = 0.01;
+    vec2 w = vec2( dist2D, abs(op.y) - h );
+    float d =  min(max(w.x,w.y),0.0) + length(max(w,0.0));
 
 	return SDFResult(
 		d,
