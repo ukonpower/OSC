69994cf615dedd89d0152889f115ac823aed2472
ukonpower <ore.ukonpower@gmail.com>
2025-10-23 00:42:58 +0900

    feat: Enhance Sashimi shader with subsurface scattering and noise texture adjustments
    - Added a new `subsurface` function to improve rendering realism through subsurface scattering effects.
    - Introduced additional noise texture sampling for more dynamic visual effects in the Sashimi shader.
    - Adjusted color and roughness calculations to incorporate new noise texture influences, enhancing overall material appearance.

--- src/resources/Components/Demo4/Maguro/Sashimi/shaders/sashimi.fs
+++ src/resources/Components/Demo4/Maguro/Sashimi/shaders/sashimi.fs
@@ -20,11 +20,13 @@ SDFResult D( vec3 p ) {
 	sashimiP.y -= 0.2;
 	
 	vec4 n = texture( uNoiseTex, p.xz * 0.1 - vec2( 0.1, 0.3 ) );
+	vec4 n2 = texture( uNoiseTex, p.xz * 2.0 - vec2( 0.1, 0.3 ) );
 	
 	vec3 pp = sashimiP;
 	pp.yz *= rotate( smoothstep( 0.0, 1.0, abs(pp.z) ) * sign( pp.z ) * 0.5);
 	pp.xy *= rotate( -smoothstep( 0.0, 0.4, abs(pp.x) ) * sign( pp.x ) * 0.5);
-	vec3 sashimiSize = vec3( 0.2, 0.02 + n.x * 0.08, 0.65 );
+	pp.y += n2.x * 0.01;
+	vec3 sashimiSize = vec3( 0.2, 0.01 + n.x * 0.08, 0.65 );
 	vec2 d = vec2( sdBox( pp, sashimiSize ), 0.0 );
 
 	vec3 trimP = pp;
@@ -38,6 +40,19 @@ SDFResult D( vec3 p ) {
 		d.y
 	);
 
+}
+
+float subsurface(vec3 ro, vec3 rd, float ra) {
+    float res = 0.;
+    const int N = 8; // samples
+    
+    for (int i=0; i<N; i++) {
+        float h = ra * float(i)/float(N); // sampling distance
+        res += clamp(D(ro + rd*h).d / h,0.,1.);
+    }
+    res /= float(N);
+    
+    return res*res*(3.-2.*res); // S curve
 }
 
 #include <rm_normal>
@@ -68,19 +83,27 @@ void main( void ) {
 
 	if( !hit ) discard;
 
+	vec4 n = texture( uNoiseTex,  rayPos.xz * 0.1 + 0.5 );
+	vec4 n2 = texture( uNoiseTex,  rayPos.xz * 4.0 + 0.5 );
+
+	float sss = subsurface( rayPos, normalize( vec3( 1.0, 1.0, 0.0 ) ), 0.15);
+
 	outNormal = N( rayPos, 0.01 );
+	outNormal = normalize( outNormal + (n2.y - 0.5) * 0.4 );
 
 	#include <rm_out_obj>
 
 	// 刺身のカラー（マグロの赤身）
-	vec3 sashimiColor = vec3( 0.9, 0.3, 0.3 );
+	vec3 sashimiColor = vec3( 0.9, 0.15, 0.1 );
 	outColor.xyz = sashimiColor;
+	outColor.xyz = mix( outColor.xyz, vec3( 1.0 ), smoothstep( 0.8, 1.0, fract(length( rayPos.xz + 0.5 + n.xy * 0.3 ) * 5.0 ) ) * n.y * 0.8 );
+	outEmission.xyz += vec3( 0.9, 0.1, 0.2 ) * sss * 0.5;
 	outRoughness = 0.4;
 
 	// グラデーション効果
 	outColor.xyz *= smoothstep( 1.5, 0.4, length( rayPos ) );
-	outRoughness = 0.2;
-	outMetalic = 0.2;
+	outRoughness = 0.2 + n2.y * 0.2;
+	outMetalic = 0.3;
 
 	#include <frag_out>
 
