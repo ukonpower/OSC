8325b2a8b56237eb7ecfbc7b7e02f14ee7e07882
ukonpower <ore.ukonpower@gmail.com>
2025-11-26 18:39:36 +0900

    style: OutroロゴのSVGデザインを更新
    OREngineロゴの新しいデザインを実装。viewBoxとパス要素を更新し、より洗練されたロゴ表示に変更。

--- src/resources/Components/Demo4/Outro/Logo/index.ts
+++ src/resources/Components/Demo4/Outro/Logo/index.ts
@@ -26,15 +26,19 @@ export class Logo extends MXP.Component {
 		// SVG要素の作成
 		this.svgWrap = document.createElement( "div" );
 		this.svgWrap.innerHTML = `
-<svg width="100%" viewBox="0 0 512 512" fill="none" stroke="white" stroke-linecap="round">
-	<circle id="ic_body" cx="256" cy="234" r="137" stroke="white" stroke-width="20"/>
-	<path id="ic_yoji" stroke-width="18" d="M413 43L289 233" />
-	<path id="ic_mayo" stroke-width="18" d="M71 246.5C84.4902 223.826 118.233 171 151.284 171C192.598 171 158.029 254 186.696 254C215.363 254 229.5 182.5 274 178.5"/>
-		<g id="ic_eye" fill="white">
-			<circle cx="216.5" cy="144" r="9"/>
-			<circle cx="249.5" cy="144" r="9"/>
-		</g>
+<svg width="503" height="154" viewBox="0 0 503 154" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M497.5 148C497.5 146.096 497.5 144.709 497.5 143" stroke="white" stroke-width="10" stroke-linecap="round"/>
+<circle cx="497.5" cy="129" r="5" fill="white"/>
+<path d="M339.5 131C340.762 132.856 341.5 135.099 341.5 137.514C341.5 143.894 336.345 149.078 330 149.078C323.655 149.078 318.5 143.894 318.5 137.514C318.5 135.099 319.238 132.856 320.5 131" stroke="white" stroke-width="8" stroke-linecap="round"/>
+<path d="M478.838 114C469.49 133.508 449.552 147 426.5 147C394.5 147 368.5 121 368.5 89C368.5 57 394.5 31 426.5 31C449.552 31 469.49 44.4921 478.838 64" stroke="white" stroke-width="12" stroke-linecap="round"/>
+<path d="M304.176 64C294.828 44.4921 274.89 31 251.838 31C228.786 31 208.848 44.4921 199.5 64C168.501 138.5 334.501 42.5 304.176 114C294.828 133.508 274.89 147 251.838 147C228.786 147 208.848 133.508 199.5 114" stroke="white" stroke-width="12" stroke-linecap="round"/>
+<path d="M85.2686 30.7422C117.158 30.7422 143.08 56.7857 143.08 89C143.08 121.214 117.158 147.258 85.2686 147.258C53.3796 147.258 27.4571 121.214 27.457 89C27.457 56.7857 53.3795 30.7422 85.2686 30.7422Z" stroke="white" stroke-width="12"/>
+<path d="M153.5 6L99.4767 88.7777" stroke="white" stroke-width="12" stroke-linecap="round"/>
+<path d="M4.5 94.6995C10.3715 84.9472 25.058 62.2266 39.4432 62.2266C57.4247 62.2266 42.3789 97.9253 54.8559 97.9253C67.3329 97.9253 73.486 67.1728 92.8543 65.4523" stroke="white" stroke-width="9" stroke-linecap="round"/>
+<circle cx="68.311" cy="50.1802" r="4.01611" fill="white"/>
+<circle cx="81.6997" cy="50.1802" r="4.01611" fill="white"/>
 </svg>
+
 `;
 
 		// スタイル設定
