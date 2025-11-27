fa6204f0ac732678dec3f190f9884da31ad44c27
ukonpower <ore.ukonpower@gmail.com>
2025-11-27 11:38:25 +0900

    feat: HUDコンポーネントにグリッドエフェクトを追加
    グローバルuniforms（uTime、uResolution）を利用して、回転する
    アニメーション付きグリッドを実装。updateImplメソッドでグローバル
    uniformsをマージする処理を追加。

--- src/resources/Components/Demo4/Common/HUD/shaders/hud.fs
+++ src/resources/Components/Demo4/Common/HUD/shaders/hud.fs
@@ -1,12 +1,38 @@
 #include <common>
 #include <packing>
 #include <frag_h>
+#include <rotate>
+
+// uTime is available from <common>
+uniform float uTimeE;
 
 void main( void ) {
 
 	#include <frag_in>
 
-	outColor.w *= 0.0;
+	float aspectRatio = uResolution.x / uResolution.y;
+
+	// grid
+
+	vec2 cuv = vUv - 0.5;
+
+	vec2 gridUv = cuv;
+	gridUv.x *= aspectRatio;
+	gridUv.xy *= rotate( 0.09 );
+	vec2 baseGridUv = gridUv;
+	gridUv.x += uTimeE * 0.05 * sign( baseGridUv.y );
+	gridUv.y *= 0.5;
+	gridUv *= 30.0; 
+	gridUv.y += floor(gridUv.x) * 0.5;
+	gridUv = fract( gridUv );
+
+	float w = 0.0;
+	w = step( 0.5, gridUv.y );
+
+	w *= step( 0.45, abs(baseGridUv.y) );
+
+	outColor.xyz = vec3( 1.0 );
+	outColor.w = w * 0.4;
 	
 	#include <frag_out>
 
