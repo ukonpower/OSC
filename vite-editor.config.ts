8f716980f05d1fbf99a9e31b9bb5e0ceb9224d11
ukonpower <ore.ukonpower@gmail.com>
2025-12-15 22:20:46 +0900

    feat: エディタページのstatic deploy設定を追加
    - vite-editor.config.tsを追加し、出力先をpages/editorに設定
    - package.jsonにbuild:editorスクリプトを追加
    - pages/editorにビルド結果を含める（static deploy用）

---
+++ vite-editor.config.ts
@@ -0,0 +1,66 @@
+import path from 'path';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+import { FileWriter } from './plugins/FileWriter';
+import { HotGetRemover } from './plugins/HotGetRemover';
+import { OREngineFileSystemPlugin } from './plugins/OREngineFileSystem';
+import { ResourceManager } from './plugins/ResourceManager';
+import { ShaderComponentRegistry } from './plugins/ShaderComponentRegistry';
+import { ShaderLoader } from "./plugins/ShaderLoader";
+
+
+const basePath = process.env.BASE_PATH ?? "";
+
+// https://vitejs.dev/config/
+export default defineConfig( {
+	root: 'src',
+	publicDir: 'assets',
+	base: basePath,
+	server: {
+		port: 3000,
+		host: "0.0.0.0",
+		watch: {
+			ignored: [ "**/data/**/**.json" ],
+		},
+		hmr: {
+			overlay: false,
+		},
+	},
+	build: {
+		outDir: '../pages/editor',
+		rollupOptions: {
+			input: './src/index.html'
+		}
+	},
+	resolve: {
+		alias: {
+			"glpower": path.join( __dirname, "packages/glpower/src" ),
+			"maxpower": path.join( __dirname, "packages/maxpower" ),
+			"orengine": path.join( __dirname, "packages/orengine" ),
+			"~": path.join( __dirname, "src" ),
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: "modern-compiler",
+			},
+		},
+	},
+	plugins: [
+		HotGetRemover(),
+		FileWriter(),
+		react(),
+		OREngineFileSystemPlugin(),
+		ShaderLoader( {
+			skipMinifier: process.env.SKIP_SHADER_MINIFIER === 'true',
+		} ),
+		ResourceManager(),
+		ShaderComponentRegistry(),
+	],
+	define: {
+		BASE_PATH: `"${basePath}"`,
+	}
+} );
